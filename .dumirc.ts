6283e4b1605b2340eeb5c662f92edadba11d5964
unfetteredman <weixingwang6032@dingtalk.com>
2023-11-16 11:20:42 +0800

    feat: set build static path

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'dist',
+  base: 'dist',
+  publicPath: '/dist/',
   themeConfig: {
     name: 'novaKit',
   },
+  exportStatic: {},
 });
